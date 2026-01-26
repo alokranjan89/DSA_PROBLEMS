2bf8fbfab4268f8855537c635433f970db9a3d86
Alok Ranjan <alokr5393@gmail.com>
2026-01-26 21:16:51 +0530

    searchInROtatedSortedArray-1

---
+++ Binary_Search/searchInRotatedArrayI.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int low = 0, high = arr.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[low] <= arr[mid]) { // Left part is sorted
+                if (arr[low] <= target && target <= arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else { // Right part is sorted
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
