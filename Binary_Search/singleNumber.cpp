649e3c1058d1166c747cc54fd8f1094b66fa5079
Alok Ranjan <alokr5393@gmail.com>
2026-01-27 19:33:15 +0530

    singleNumber

---
+++ Binary_Search/singleNumber.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int singleNonDuplicate(vector<int>& nums) {
+    int n = nums.size();
+
+    if(n == 1) return nums[0];
+    if(nums[0] != nums[1]) return nums[0];
+    if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+    int low = 1;
+    int high = n - 2;
+
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // If mid is the single element
+        if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) {
+            return nums[mid];
+        }
+
+        // Check which half is valid
+        if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
+           (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+            low = mid + 1;   // single element on right
+        } else {
+            high = mid - 1;  // single element on left
+        }
+    }
+
+    return -1;
+}
