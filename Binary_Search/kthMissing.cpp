a82511833aa4447164009ff97b9895157d41f24d
Alok Ranjan <alokr5393@gmail.com>
2026-01-30 16:47:40 +0530

    kthMissing

---
+++ Binary_Search/kthMissing.cpp
@@ -0,0 +1,30 @@
+/*
+
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findKthPositive(vector<int>& arr, int k) {
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        int missing = arr[mid] - (mid + 1);
+
+        if (missing < k) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return low + k;
+}
